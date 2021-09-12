6d41637e45373ed4d3bbe813df1e98556ebf72c1
dezhizhang <1541609448@qq.com>
2021-09-12 17:40:00 +0800

    feat:提交

--- README.md
+++ README.md
@@ -4,3 +4,24 @@
 ```
 driver.DB.Where("id > ? and id < ?",1,4).Find(&navList)
 ```
+2,在什么之前的查询
+```
+driver.DB.Where("id in (?)",[]int{2,4}).Find(&navList)
+```
+3,模源匹配
+```
+driver.DB.Where("title like ? ","%科技%").Find(&navList)
+```
+4,两者之间
+```
+driver.DB.Where("id between ? and ?",2,4).Find(&navList)
+```
+5,等于其中的一个数
+```
+driver.DB.Where("id=? or id=?",2,3).Find(&navList)
+```
+6, 排序
+```
+driver.DB.Order("id desc").Find(&navList)
+```
+
--- controller/back/nav.go
+++ controller/back/nav.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"saas/driver"
 	"saas/models"
 )
 
@@ -18,7 +17,13 @@ func (n Nav) List(c *gin.Context)  {
 	fmt.Println("id",id)
 
 	//driver.DB.Where("id > ?",3).Find(&navList)
-	driver.DB.Where("id > ? and id < ?",1,4).Find(&navList)
+	//driver.DB.Where("id > ? and id < ?",1,4).Find(&navList)
+	//driver.DB.Where("id in (?)",[]int{2,4}).Find(&navList)
+	//driver.DB.Where("title like ? ","%科技%").Find(&navList)
+	//driver.DB.Where("id between ? and ?",2,4).Find(&navList)
+	//driver.DB.Where("id=? or id=?",2,3).Find(&navList)
+	//driver.DB.Select("id,title").Find(&navList)
+	//driver.DB.Order("id desc").Find(&navList)
 	c.JSON(http.StatusOK,gin.H{
 		"code":200,
 		"msg":"ok",
