7f742459efc7a56cbdc3839fadabcc3012702cd9
dezhizhang <1541609448@qq.com>
2021-09-12 17:13:31 +0800

    feat:查询

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # saas
-saas
+### sql查询
+1,两个数之前的查询
+```
+driver.DB.Where("id > ? and id < ?",1,4).Find(&navList)
+```
--- controller/back/nav.go
+++ controller/back/nav.go
@@ -1,6 +1,7 @@
 package back
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"saas/driver"
@@ -13,7 +14,11 @@ type  Nav struct {
 
 func (n Nav) List(c *gin.Context)  {
 	var navList []models.Nav
-	driver.DB.Find(&navList)
+	id := c.GetInt("id")
+	fmt.Println("id",id)
+
+	//driver.DB.Where("id > ?",3).Find(&navList)
+	driver.DB.Where("id > ? and id < ?",1,4).Find(&navList)
 	c.JSON(http.StatusOK,gin.H{
 		"code":200,
 		"msg":"ok",
