b5d52288c397c022d170b74b90e4c38b1d73833f
dezhizhang <1541609448@qq.com>
2021-10-01 16:40:46 +0800

    feat:生成md5

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,21 @@
 package utils
 
-import "time"
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"time"
+)
 
 // 时间戳转换成日期
 
 func UnixToTime(timestamp int) string {
 	t := time.Unix(int64(timestamp), 0)
 	return t.Format("2006-01-02 15:04:05")
+}
+
+func Md5(str string) string {
+	h := md5.New()
+	io.WriteString(h,str)
+	return fmt.Sprintf("%x",h.Sum(nil))
 }
