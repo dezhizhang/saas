691cc907126bfdb4e994679e46f314845ff86378
dezhizhang <1541609448@qq.com>
2021-11-27 20:16:20 +0800

    feat:添加模板

--- controller/front/home.go
+++ controller/front/home.go
@@ -2,34 +2,18 @@ package front
 
 import (
 	"context"
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
-	"saas/driver"
+
+	"github.com/gin-gonic/gin"
 )
 
 var ctx = context.Background()
 
 type Home struct {
-
 }
 
-func (h Home) Home(c *gin.Context)  {
-	err := driver.RDB.Set(ctx,"name","张三",1).Err()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	val,err := driver.RDB.Get(ctx,"name").Result()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("val",val)
-
-	c.JSON(http.StatusOK,gin.H{
-		"code":200,
-		"msg":"hello world",
-	})
+func (h Home) Home(c *gin.Context) {
+	c.HTML(http.StatusOK, "front/home/index.html", gin.H{})
 }
+
+//c.HTML(http.StatusOK,"back/login/index.html",gin.H{})
