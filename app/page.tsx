f58be50e2aa0cdb8f8673e03525adca4edd0f6ee
dezhizhang <1541609448@qq.com>
2024-08-09 15:46:19 +0800

    feat:提交

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
-  return <Button variant="outline" size="sm">按钮</Button>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <form className="w-full max-w-md flex flex-col gap-4">
+        <h1 className="text-center text-2xl">晓智科技</h1>
+        <Input type="text" placeholder="请输入用户名" />
+        <Textarea placeholder="请输入" />
+        <Button type="submit">登录</Button>
+      </form>
+    </div>
+  );
 }
